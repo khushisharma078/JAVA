1716e435ea12aa76dd8f7a95e45025dc115626f3
khushisharma078 <141501253+khushisharma078@users.noreply.github.com>
2023-08-05 19:01:35 +0530

    Create GREATER USING TERNARY.JAVA

---
+++ GREATER USING TERNARY.JAVA
@@ -0,0 +1,12 @@
+import java.util.*;
+class Greater
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int k=(a>b)?a:b;
+        System.out.println(k);
+    }
+}
