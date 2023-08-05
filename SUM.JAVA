6cd58f7a59b557423242eed9b6f833f0ed04a441
khushisharma078 <141501253+khushisharma078@users.noreply.github.com>
2023-08-05 18:59:43 +0530

    Create SUM.JAVA

---
+++ SUM.JAVA
@@ -0,0 +1,13 @@
+import java.util.*;
+public class sum 
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter 2 nos.");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=a+b;
+        System.out.println("Sum of 2 nos is "+c);
+    }
+}
